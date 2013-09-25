b1ad7028bfbced11fff5cb009416e3fc1e42eabf
cometsong <benjamin@cometsong.net>
2013-09-25 09:06:24 +0200

    Initial script and test folder commit.

---
+++ tests/ln-create.sh
@@ -0,0 +1,5 @@
+mkdir -p usr/local/{bin,share,man,sbin,lib,include,etc}
+mkdir -p usr/local/{s1,s2,s3}/{bin,share,man,sbin,lib,include,etc}
+touch usr/local/s1/{bin,share,man,sbin,lib,include,etc}/s1file
+touch usr/local/s2/{bin,share,man,sbin,lib,include,etc}/s2file
+touch usr/local/s3/{bin,share,man,sbin,lib,include,etc}/s3file
